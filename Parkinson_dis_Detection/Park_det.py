f66ed327a4b046022ccdb4cb54c18915096efcea
Shivram-U <udayakumar933@gmail.com>
2024-02-07 15:11:51 +0530

    update

---
+++ Parkinson_dis_Detection/Park_det.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import os, sys
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+#DataFlair - Read the data
+
+#DATASET_DIRECTORY
+df=pd.read_csv("D:\\Software\\Work_Spaces\\Workspace\\Machine_Learning\\Parkinson_dis_Detection\\parkinsons.data")
+df.head()
+
+#DataFlair - Get the features and labels
+features=df.loc[:,df.columns!='status'].values[:,1:]
+labels=df.loc[:,'status'].values
+
+#DataFlair - Get the count of each label (0 and 1) in labels
+print(labels[labels==1].shape[0], labels[labels==0].shape[0])
+
+#DataFlair - Scale the features to between -1 and 1
+scaler=MinMaxScaler((-1,1))
+x=scaler.fit_transform(features)
+y=labels
+
+#DataFlair - Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(x, y, test_size=0.2, random_state=7)
+
+#DataFlair - Train the model
+model=XGBClassifier()
+model.fit(x_train,y_train)
+
+# DataFlair - Calculate the accuracy
+y_pred=model.predict(x_test)
+print(accuracy_score(y_test, y_pred)*100)
